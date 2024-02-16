d1a65a69588dbd0d3a735c3541c2178534972126
asmitaj08 <asmita.jha82@gmail.com>
2024-02-15 17:44:48 -0800

    updated harness script

--- fuzzers/libafl_renode/harness_script.py
+++ fuzzers/libafl_renode/harness_script.py
@@ -5,10 +5,12 @@
 
 import ctypes
 import subprocess
+from pyrenode3.wrappers import Analyzer, Emulation, Monitor
+from Antmicro.Renode.Peripherals.CPU import TranslationCPUHooksExtensions
 
 
-libafl_renode_lib = ctypes.CDLL("/home/asmita/fuzzing_bare-metal/libafl_modify/fuzzers/libafl_renode/target/release/liblibafl_renode.so")
-input_dir = "/home/asmita/fuzzing_bare-metal/libafl_modify/fuzzers/libafl_renode/input_dir"
+libafl_renode_lib = ctypes.CDLL("/home/asmita/fuzzing_bare-metal/SEFF_project_dirs/SEFF-project/LibAFL/fuzzers/libafl_renode/target/release/liblibafl_renode.so")
+input_dir = "/home/asmita/fuzzing_bare-metal/SEFF_project_dirs/in-dir/test"
 
 # just tetsing dll import from libafl
 test_res = libafl_renode_lib.external_current_millis2()
@@ -18,8 +20,8 @@ print(test_res)
 callback_function = ctypes.CFUNCTYPE(None, ctypes.c_char_p, ctypes.c_size_t)
 
 # renode setup
-renode_path = "/home/asmita/fuzzing_bare-metal/renode_modify/renode/renode"
-resc_path = "/home/asmita/fuzzing_bare-metal/renode_modify/renode/scripts/single-node/nrf52840.resc"
+# renode_path = "/home/asmita/fuzzing_bare-metal/renode_modify/renode/renode"
+# resc_path = "/home/asmita/fuzzing_bare-metal/renode_modify/renode/scripts/single-node/nrf52840.resc"
 
 # # renode_command = [renode_path, f"-e i @{resc_path};cpu SetHookAtBlockBegin \"print '***HookFn'\";s"]
 # renode_command = [renode_path, f"-e i @{resc_path};s;sysbus.uart0 WriteChar 0x44"] # 0x44 will be replaced by data sent by LibAFL
@@ -29,22 +31,35 @@ resc_path = "/home/asmita/fuzzing_bare-metal/renode_modify/renode/scripts/single
 # subprocess.run(renode_command)
 # print("Executed renode")
 # define callback which executes renode
-state_file = "/home/asmita/fuzzing_bare-metal/libafl_modify/fuzzers/libafl_renode/statefile.dat"
+# state_file = "/home/asmita/fuzzing_bare-metal/libafl_modify/fuzzers/libafl_renode/statefile.dat"
 # renode_command = [renode_path, f"-e i @{resc_path};s;Save @{state_file};quit"]
 # subprocess.run(renode_command)
 
 # renode_command = [renode_path, f"-e i @{resc_path};Load @{state_file};mach set 0; showAnalyzer sysbus.uart0; sysbus.uart0 WriteChar {0x44}"]
 # subprocess.run(renode_command)
 
+# ***** setup_renode, currently hardcoded for nrf52840 uart
+e = Emulation()
+m = Monitor()
+nrf52840 = e.add_mach("nrf52840")
+nrf52840.load_repl("platforms/cpus/nrf52840.repl")
+nrf52840.load_elf("https://dl.antmicro.com/projects/renode/renode-nrf52840-zephyr_shell_module.elf-gf8d05cf-s_1310072-c00fbffd6b65c6238877c4fe52e8228c2a38bf1f")
+
+TranslationCPUHooksExtensions.SetHookAtBlockBegin(nrf52840.sysbus.cpu.internal, nrf52840.internal, " ")
+Analyzer(nrf52840.sysbus.uart0).Show()
+# e.StartAll()
 def callback(data, size):
     # print("Callback called")
     # data = data.strip('\n')
     # Convert raw bytes to Python string for printing
     # data_str = ctypes.string_at(data, size).decode('utf-8')
     # print("Callback data:", data)
-    renode_command = [renode_path, f"--disable-xwt --console --hide-analyzers -e i @{resc_path};s;sysbus.uart0 WriteChar {0x44};quit"]
-    subprocess.run(renode_command)
+    # renode_command = [renode_path, f"--disable-xwt --console --hide-analyzers -e i @{resc_path};s;sysbus.uart0 WriteChar {0x44};quit"]
+    # subprocess.run(renode_command)
     # print("Executed renode")
+    e.StartAll()
+    m.execute(f"sysbus.uart0 WriteChar {data}")
+    # m.execute(f"sysbus.uart0 WriteChar {0x44}")
 
 
 
