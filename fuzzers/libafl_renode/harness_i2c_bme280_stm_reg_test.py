b068e89fdc9074ae720e2febc2857ea682bc3e6c
asmitaj08 <asmita.jha82@gmail.com>
2024-07-12 05:37:17 -0700

    harness_updated

--- fuzzers/libafl_renode/harness_i2c_bme280_stm_reg_test.py
+++ fuzzers/libafl_renode/harness_i2c_bme280_stm_reg_test.py
@@ -1,7 +1,6 @@
 # !/usr/bin/env -S python3 -m bpython -i
 import time
 import timeit
-import ctypes
 from pyrenode3 import RPath
 import System   # import sys
 from pyrenode3.wrappers import Analyzer, Emulation, Monitor
@@ -22,11 +21,8 @@ e = Emulation()
 m = Monitor()
 mach = e.add_mach(mach_name)
 
-state_file= "statefile_bme280_stm_new.dat"
+state_file= "statefile_bme280_stm_reg.dat"
 
-libafl_renode_lib = ctypes.CDLL("/home/asmita/fuzzing_bare-metal/SEFF_project_dirs/SEFF-project/LibAFL/fuzzers/libafl_renode/target/release/liblibafl_renode.so")
-input_dir = "/home/asmita/fuzzing_bare-metal/SEFF_project_dirs/SEFF-project/LibAFL/fuzzers/libafl_renode/input_dir_i2c"
-callback_function = ctypes.CFUNCTYPE(None, ctypes.c_char_p)
 
 temp_data = 60
 humidity_data = 88
@@ -37,8 +33,9 @@ reach_target_flag = 0
 exit_addr = 0x08003544
 # restore_pc = 0x0800353a
 # restore_sp = 0x20001040
-load_str = """using "platforms/cpus/stm32l072.repl" bme280: I2C.BME280@ i2c1 0x76"""
-PlatformDescriptionMachineExtensions.LoadPlatformDescriptionFromString(mach.internal,load_str)
+# load_str = """using "platforms/cpus/stm32l072.repl" bme280: I2C.BME280@ i2c1 0x76"""
+# PlatformDescriptionMachineExtensions.LoadPlatformDescriptionFromString(mach.internal,load_str)
+mach.load_repl("platforms/cpus/stm32l072_modified.repl")
 mach.load_elf("https://dl.antmicro.com/projects/renode/b_l072z_lrwan1--zephyr-bme280_test.elf-s_649120-15b7607a51b50245f4500257c871cd754cfeca5a")
 
 pc_main = mach.sysbus.GetSymbolAddress("main")
@@ -51,12 +48,12 @@ def hook_addr_target(cpu,addr):
     # sp_target = mach.sysbus.cpu.GetRegisterUnsafe(13).RawValue  
     mach.sysbus.cpu.Pause()
     reach_target_flag = 1
-    print("**Target Hook task done")
+    print("Target Hook task done")
 
 def hook_addr_exit(cpu,addr):
     global exit_flag
     exit_flag = 1
-    # print("***** Exit addr *******")
+    print("***** Exit addr *******")
 
 Action3 = getattr(System, 'Action`2')
 hook_action_target = Action3[ICpuSupportingGdb, System.UInt64](hook_addr_target)
@@ -64,8 +61,6 @@ mach.sysbus.cpu.AddHook(target_func_calling_pc,hook_action_target)
 
 Action4 = getattr(System, 'Action`2')
 hook_action_exit = Action4[ICpuSupportingGdb, System.UInt64](hook_addr_exit)
-
-TranslationCPUHooksExtensions.SetHookAtBlockBegin(mach.sysbus.cpu.internal, mach.internal, " ")
 
 Analyzer(mach.sysbus.usart2).Show()
 e.StartAll()
@@ -85,30 +80,26 @@ load_path_format = ReadFilePath(state_file)
 print("Done initial setup")
 
 
-def callback(data) :
-    global exit_flag,e
-    # print("Loading the saved file")
+while 1 :
+    print("Loading the saved file")
     # start_time = time.time()
     EmulationManager.Instance.Load(load_path_format)
     # end_time = time.time()
     # load_execution_time = end_time - start_time
     # print(f"Execution time for the line: {load_execution_time:.10f} seconds")
-    # print("file loaded")
+    print("file loaded")
     mach_new = e.get_mach(mach_name)
     mach_new.sysbus.cpu.AddHook(exit_addr,hook_action_exit)
     # print(f"***Mach here : {mach_new}")
-    # print(f"******PC : {mach_new.sysbus.cpu.PC}")
-    if len(data)==0 :
-        data=[0x20]
-    # Analyzer(mach_new.sysbus.usart2).Show()
-    Analyzer(mach_new.sysbus.usart2)
-    # temp_data += 20
-    # humidity_data += 20
-    # pressure_data += 20
-    mach_new.sysbus.i2c1.bme280.Temperature = data[0]
-    mach_new.sysbus.i2c1.bme280.Humidity = data[0]
-    mach_new.sysbus.i2c1.bme280.Pressure = data[0]
-    # print("Resuming next run")
+    print(f"******PC : {mach_new.sysbus.cpu.PC}")
+    Analyzer(mach_new.sysbus.usart2).Show()
+    temp_data += 20
+    humidity_data += 20
+    pressure_data += 20
+    mach_new.sysbus.i2c1.bme280.Temperature = temp_data
+    mach_new.sysbus.i2c1.bme280.Humidity = humidity_data
+    mach_new.sysbus.i2c1.bme280.Pressure = pressure_data
+    print("Resuming next run")
     e.StartAll()
     # print(f"starting new run: Temp : {mach.sysbus.i2c1.bme280.Temperature}, Hum : {mach.sysbus.i2c1.bme280.Humidity}, Pres : {mach.sysbus.i2c1.bme280.Pressure}")
     # print(f"Reg set to : pc : {hex(mach.sysbus.cpu.GetRegisterUnsafe(15).RawValue)}, sp : {hex(mach.sysbus.cpu.GetRegisterUnsafe(13).RawValue)}, lr : {hex(mach.sysbus.cpu.GetRegisterUnsafe(14).RawValue)}")
@@ -123,9 +114,6 @@ def callback(data) :
     # time.sleep(1)
     # e.clear()
 
-callback_ptr = callback_function(callback)
-print("calling liabafl main_fuzzing_func")
-libafl_renode_lib.main_fuzzing_func(ctypes.c_char_p(input_dir.encode('utf-8')),callback_ptr)
 
 
 
